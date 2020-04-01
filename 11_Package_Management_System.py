09d54f291b68acb9e8f2c8c316468ba078641cf5
Kiran babu <51410158+RouthKiranbabu@users.noreply.github.com>
2020-04-01 12:59:59 +0530

    Create 11_Package_Management_System.py

---
+++ 11_Package_Management_System.py
@@ -0,0 +1,32 @@
+'''
+This whole command is no a python programming and this code is writen in command prompt
+
+command for help
+pip help
+
+#To check all the modules
+pip install
+
+Command that helps for installation
+pip help install
+
+To search the module and know its description
+This may not be available or install by having interner it does just above work
+pip search Pympler
+
+To install any module
+pip install Pympler
+
+To uninstall any module
+pip uninstall Pympler
+
+To check outdated version of every module
+pip list -o
+
+To update a package
+pip install -U django-taggit
+
+After updating django-taggit is not listed on command output named: pip list -o
+
+To check its new version go for list command: pip list
+
