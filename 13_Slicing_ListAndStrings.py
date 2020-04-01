f7ab08f72cc4d421cfa12b8500e3c22df0aca45c
Kiran babu <51410158+RouthKiranbabu@users.noreply.github.com>
2020-04-01 13:00:06 +0530

    Create 13_Slicing_ListAndStrings.py

---
+++ 13_Slicing_ListAndStrings.py
@@ -0,0 +1,27 @@
+my_List=[0,1,2,3,4,5,6,7,8,9]
+#       (0,1,2,3,4,5,6,7,8,9)
+#     -(10,9,8,7,6,5,4,3,2,1)
+
+#Print element by its index
+print(my_List[0],my_List[-10])
+
+#Printing range having default step=1
+#Printing element from index zero to 4 terminating point is not inclusive
+print(my_List[0:5],my_List[-10:-5])
+
+#Printing element from positive 0 index to positive 9 index or -1 index
+print(my_List[:-1])
+
+#Printing through step
+print(my_List[0:10:2])
+
+#Negitive step
+print(my_List[-1:0:-2])
+
+#Reversing the list
+print(my_List[::-1])
+
+#For reversing the string
+String="My name is kiran"
+print(String[::-1])
+
